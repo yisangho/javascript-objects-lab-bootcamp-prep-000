b10380eb704bae1cb2826e3e552bfa393c984209
yisangho <yisangho717@gmail.com>
2018-08-25 19:23:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function destructivelyUpdateObjectWithKeyAndValue(obj, key, value) {
 }
 
 function destructivelyDeleteFromObjectByKey(obj, key) {
-  obj = obj[key]
+  delete obj.key
 console.log(obj)
 console.log("=================================")
 
