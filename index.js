7001fe386c98d8f798dd91364efe5ce24441eae2
yisangho <yisangho717@gmail.com>
2018-08-25 18:54:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 var recipes = {};
 
-function updateObjectWithKeyAndValue(object, key, value) {
-  object[key] = value
-  return object
+function updateObjectWithKeyAndValue(obj, key, value) {
+  obj[key] = value
+  return obj
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
