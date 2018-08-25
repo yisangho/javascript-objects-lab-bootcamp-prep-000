4eee11bfa4988d4b621d5d3df9f5ed301794eb41
yisangho <yisangho717@gmail.com>
2018-08-25 18:52:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 var recipes = {};
 
 function updateObjectWithKeyAndValue(object, key, value) {
-  return object[key] = value
+  object[key] = value
+  return object
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
-  return delete object.key
+  delete object[key]
+  return object
 }
 
 function destructivelyDeleteFromObjectByKey(object, key) {
